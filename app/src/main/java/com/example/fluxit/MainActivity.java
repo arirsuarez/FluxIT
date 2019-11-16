0f862f6077fb38852881369a8b013a2d7c9b1cf0
AriS <arielr.suarez@gmail.com>
2019-11-16 15:20:52 -0300

    Setting SearchView on Toolbar. TODO: AboutMe

--- app/src/main/java/com/example/fluxit/MainActivity.java
+++ app/src/main/java/com/example/fluxit/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.mainActivityToolbar)
     Toolbar mainActivityToolbar;
+    @BindView(R.id.searchview_action)
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +38,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
+        searchView.setMenuItem(menu.findItem(R.id.search_action));
         return true;
     }
 
     // Setting actions for toolbar buttons
-    // TODO Set SearchView & AboutMe
+    // TODO AboutMe
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         String actionIName = (String) item.getTitle();
         Toast.makeText(getApplicationContext(), "Selected option: " + actionIName, Toast.LENGTH_SHORT).show();
 
         switch (item.getItemId()) {
-            case R.id.search_action:
-                break;
             case R.id.about_me_action:
                 break;
         }
